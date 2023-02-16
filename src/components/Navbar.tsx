e4fcc2c0f7e310980f5ba574f8b06f27c545cea8
Dev.Prabhat <76727816+Prabhat2373@users.noreply.github.com>
2023-02-16 15:30:44 +0530

    navbar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <>
+            <header>
+                <nav className="grid grid-cols-6 border border-border-main ">
+                    <div className='col-span-1 items-center self-center'>
+                        <h1 className='w-28 text-text-main '>dev-prabhat</h1>
+                    </div>
+                    <div className='col-span-4'>
+                        <ul className='flex'>
+                            <li className='flex justify-center items-center cursor-pointer text-white px-6 border border-border-main py-4 border-b-2 border-b-bg-main hover:border-b-yellow'><a href="">_hello</a></li>
+                            <li className='flex justify-center items-center cursor-pointer text-text-main px-6 border border-border-main py-4 border-b-2 border-b-bg-main hover:border-b-yellow'><a href="">_about-me</a></li>
+                            <li className='flex justify-center items-center cursor-pointer text-text-main px-6 border border-border-main py-4 border-b-2 border-b-bg-main hover:border-b-yellow'><a href="">_projects</a></li>
+                        </ul>
+                    </div>
+                    <div className='py-4 px-6 border border-border-main'>
+                        <a className='text-text-main  ' href="">contact</a>
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
 }
 
 export default Navbar
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
+    colors: {
+      'bg-main': '#011627',
+      'border-main': '#1E2D3D',
+      'text-main': '#607B96',
+      'white': '#ffffff',
+      'yellow': '#FEA55F',
+      'hero-blue': '#4D5BCE',
+      'teal': '#E99287',
+      'cyan-green': '#43D9AD',
+    },
   },
-  plugins: [],
+  plugins: [
+  ],
 }
